11d89be890768a1a9231a6c8932e8fcbd801e3e3
Thanh-Cong0602 <cong.nguyen0602@hcmut.edu.vn>
2023-07-04 11:29:54 +0700

    My Fix

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { Person, Lock } from 'react-bootstrap-icons';
 import { userActions } from '../../Redux/_actions';
