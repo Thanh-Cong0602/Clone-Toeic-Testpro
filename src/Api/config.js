e819865ed9edba71190f6956fc12c34ddedbee3f
Thanh-Cong0602 <cong.nguyen0602@hcmut.edu.vn>
2023-07-05 21:08:20 +0700

    Completed LoginPage

---
+++ src/Api/config.js
@@ -0,0 +1,4 @@
+const API_BASE = {
+   apiUrl: "https://toeic-exam-web.onrender.com/api/v1",
+}
+export default API_BASE
--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import HomePage from './Pages/HomePage/HomePage';
+import LoginPage from './Pages/LoginPage/LoginPage';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+
 function App() {
+   const isLoggedIn = useSelector(state => state.authentication.isLoggedIn);
    return (
       <div className="App">
          <BrowserRouter>
             <Header />
             <Navbar />
-            <HomePage />
+            <Routes>
+               <Route path="/" element={<HomePage />} />
+            </Routes>
+            <Routes >
+               {/* <Route path="/login" element={isLoggedIn ? <Navigate to="/" replace /> : <LoginPage />} />
+                */}
+               <Route path="/login" element={<LoginPage />} />
+            </Routes>
             <Footer />
          </BrowserRouter>
       </div>
