d52104ce76fcc335441ef85876456ccf80c59a2a
Thanh-Cong0602 <cong.nguyen0602@hcmut.edu.vn>
2023-07-05 14:51:14 +0700

    Completed initial Main

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter} from 'react-router-dom';
-
-import HomePage from './Pages/HomePage/HomePage';
-import Footer from './Components/Footer/Footer';
-import Header from './Components/Header/Header';
-import Navbar from './Components/Navbar/Navbar';
 
 function App() {
    return (
       <div className="App">
          <BrowserRouter>
-            <Header />
-            <Navbar />
-            <HomePage />
-            <Footer />
+           My App
          </BrowserRouter>
       </div>
    );
