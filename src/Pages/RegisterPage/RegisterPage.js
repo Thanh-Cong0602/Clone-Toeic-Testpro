fcc4bda7eea18680c855988833512b9431c1f37f
Thanh-Cong0602 <cong.nguyen0602@hcmut.edu.vn>
2023-07-14 11:00:49 +0700

    Process

--- src/Pages/RegisterPage/RegisterPage.js
+++ src/Pages/RegisterPage/RegisterPage.js
@@ -20,13 +20,74 @@ function RegisterPage() {
          age: 0
       }
    );
+   const [errors, setErrors] = useState({
+      username: '',
+      password: '',
+      confirmPassword: '',
+      surname: '',
+      name: '',
+      email: '',
+      address: '',
+      age: '',
+      phone: ''
+   })
+   const hasSpecialCharacters = (input) => {
+      const regex = /[!@#$%^&*(),.?":{}|<>]/;
+      return regex.test(input);
+   };
+   const isValidEmail = (email) => {
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return regex.test(email);
+   };
+   const isValidPhoneNumber = (phoneNumber) => {
+      const regex = /([\+84|84|0]+(3|5|7|8|9|1[2|6|8|9]))+([0-9]{8})\b/
+      return regex.test(phoneNumber);
+   };
    const [hasEnteredAge, setHasEnteredAge] = useState(false);
    const navigate = useNavigate();
+   const setError = (fileName, errorMessage) => {
+      setErrors((error) => ({
+         ...error,
+         [fileName]: errorMessage
+      }))
+   }
    function handleChange(e) {
       const { name, value } = e.target;
-      setUser(user => ({ ...user, [name]: name == 'age' ? parseInt(value) : value }));
+      setUser(user => ({ ...user, [name]: name === 'age' ? parseInt(value) : value }));
       if (name === 'age') {
          setHasEnteredAge(true);
+      }
+      if (name === 'username') {
+         let errorMessage = hasSpecialCharacters(value) ? 'Username cannot contain special characters.' : ''
+         setError('username', errorMessage)
+      }
+      else if (name === 'password') {
+         let errorMessage = (value.length < 8) ? 'Password must be at least 8 characters long.' : ''
+         setError('password', errorMessage)
+      }
+      else if (name === 'confirmPassword') {
+         let errorMessage = (value.length < 8) ? 'ConfirmPassword must be at least 8 characters long.' : ''
+         setError('confirmPassword', errorMessage)
+      }
+      else if (name == 'surname') {
+         let errorMessage = hasSpecialCharacters(value) ? 'Surname cannot contain special characters.' : ''
+         setError('surname', errorMessage)
+      }
+      else if (name == 'name') {
+         let errorMessage = hasSpecialCharacters(value) ? 'Name cannot contain special characters.' : ''
+         setError('name', errorMessage)
+      }
+      else if (name == 'email') {
+         let errorMessage = !isValidEmail(value) ? 'Invalid Email' : ''
+         setError('email', errorMessage)
+      }
+      else if (name == 'age') {
+         let errorMessage = (value < 0) ? 'Invalid age' : ''
+         setError('age', errorMessage)
+      }
+      else if (name == 'phone') {
+         let errorMessage = ( value.length > 11 ) ? 'Invalid Phone Number' : ''
+         setError('phone', errorMessage)
       }
    }
 
@@ -63,30 +124,30 @@ function RegisterPage() {
                </div>
                <div className='text'>
                   <input type='text' placeholder='Enter username'
-                     name='username' value={user.username} onChange={handleChange} required />
+                     name='username' value={user.username} onChange={handleChange} />
                </div>
             </div>
-
+            {errors.username && <div className='error-message'>{errors.username}</div>}
             <div className='input-item'>
                <div className='icon'>
                   <Key size={25}></Key>
                </div>
                <div className='text'>
                   < input type='password' placeholder='Enter your password'
-                     name='password' value={user.password} onChange={handleChange} required />
+                     name='password' value={user.password} onChange={handleChange} />
                </div>
             </div>
-
+            {errors.password && <div className='error-message'>{errors.password}</div>}
             <div className='input-item'>
                <div className='icon'>
                   <Key size={25}></Key>
                </div>
                <div className='text'>
                   <input type='password' placeholder='Enter your confirm password'
-                     name='confirmPassword' value={user.confirmPassword} onChange={handleChange} required />
+                     name='confirmPassword' value={user.confirmPassword} onChange={handleChange} />
                </div>
             </div>
-
+            {errors.confirmPassword && <div className='error-message'>{errors.confirmPassword}</div>}
             <div className='choose-item'>
                <div>
                   <span>Choose Role</span>
@@ -109,29 +170,30 @@ function RegisterPage() {
                </div>
                <div className='text'>
                   <input type='text' placeholder='Enter your surname'
-                     name='surname' value={user.surname} onChange={handleChange} required />
+                     name='surname' value={user.surname} onChange={handleChange} />
                </div>
             </div>
-
+            {errors.surname && <div className='error-message'>{errors.surname}</div>}
             <div className='input-item'>
                <div className='icon'>
                   <Person size={25}></Person>
                </div>
                <div className='text'>
                   <input type='text' placeholder='Enter your name'
-                     name='name' value={user.name} onChange={handleChange} required />
+                     name='name' value={user.name} onChange={handleChange} />
                </div>
             </div>
-
+            {errors.name && <div className='error-message'>{errors.name}</div>}
             <div className='input-item'>
                <div className='icon'>
                   <Envelope size={25}></Envelope>
                </div>
                <div className='text'>
-                  <input type='text' placeholder='Enter your email' required
+                  <input type='text' placeholder='Enter your email'
                      name='email' value={user.email} onChange={handleChange} />
                </div>
             </div>
+            {errors.email && <div className='error-message'>{errors.email}</div>}
             <div className='dateofBirth'>
                <label for="birthday">Birthday:</label>
                <input type="date" id="birthday" name="dateOfBirth"
@@ -158,7 +220,7 @@ function RegisterPage() {
                      name='phone' value={user.phone} onChange={handleChange} />
                </div>
             </div>
-
+            {errors.phone && <div className='error-message'>{errors.phone}</div>}
             <div className='input-item'>
                <div className='icon'>
                   <PersonCheck size={25}></PersonCheck>
@@ -168,7 +230,7 @@ function RegisterPage() {
                      name='age' value={hasEnteredAge ? user.age : ''} onChange={handleChange} />
                </div>
             </div>
-
+            {errors.age && <div className='error-message'>{errors.age}</div>}
             <div className='login-btn-wrapper'>
                <input className='login-btn' type='submit' value={"REGISTER"} />
             </div>
